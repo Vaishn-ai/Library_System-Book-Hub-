28b7dc587218a1953b9cc226555c8c22932d2700
Vaishnavi dandekar <vaishnavidandekar81@gmail.com>
2026-01-23 09:31:19 +0530

    render deploy setup

--- library_pro/settings.py
+++ library_pro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-iojba7m-e(!gpo3(&pma$jn*xk9w%prv1r4+g+)n3%is+(q+m8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
 ]
 
 ROOT_URLCONF = 'library_pro.urls'
